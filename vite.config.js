7b4c8a26619c85544538ccf165cd21a5d1521241
angelkawaii <amechan@raincandy.u>
2024-02-10 22:42:34 +0800

    update pwa icon settings

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,35 @@
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { VitePWA } from 'vite-plugin-pwa'
+import {VitePWA} from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    VitePWA({
-      includeAssets: ['./assets/logo.svg'],
-      manifest: {
-        name: 'InclusiveToiletProject',
-        short_name: 'ToiletMarker',
-        description: 'InclusiveToiletProject tool',
-        theme_color: '#ffa8b8',
-        icons: [
-          {
-            src: './assets/logo.svg',
-            sizes: '192x192',
-            type: 'image/svg'
-          }
-        ]
-      }
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  base:'./'
+    plugins: [
+        vue(),
+        VitePWA({
+            includeAssets: ['./assets/logo.svg'],
+            manifest: {
+                name: 'InclusiveToiletProject',
+                short_name: 'ToiletMarker',
+                description: 'InclusiveToiletProject tool',
+                theme_color: '#ffa8b8',
+                icons: [
+                    {
+                        src: './public/logo.svg',
+                        sizes: '192x192',
+                        type: 'image/svg',
+                        purpose: "any"
+                    }
+                ]
+            }
+        })
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    base: './'
 })
