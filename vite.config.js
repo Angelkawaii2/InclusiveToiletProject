309fe63de4c0f02e54697f9c74712fe891bf812e
Jiean Ru <139684203+Angelkawaii2@users.noreply.github.com>
2024-02-10 22:27:05 +0800

    Update vite.config.js
    config pwa

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [
     vue(),
-    VitePWA()
+    VitePWA({ registerType: 'autoUpdate' })
   ],
   resolve: {
     alias: {
